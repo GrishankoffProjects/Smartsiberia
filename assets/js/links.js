d5a5a2a2644ba288e8f2138bee8b49671ebff3c5
Lex <domian844@gmail.com>
2020-09-04 15:23:11 +0700

    add

---
+++ assets/js/links.js
@@ -0,0 +1,3 @@
+document.getElementById('vk').href = "https://vk.com/grishankoff";
+document.getElementById('nvkz').href = "http://smartsiberia.net/";
+document.getElementById('sciencehome').href = "http://smartsiberia.net/";
